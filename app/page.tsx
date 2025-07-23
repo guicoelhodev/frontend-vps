39268846121ab5efed11891b6ceae5afc38e4317
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-23 20:04:32 -0300

    feat(init): add initial setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="grid w-screen h-screen place-content-center">
+			<h1 className="font-semibold text-3xl">HELLO VPS CLIENT !!</h1>
+		</div>
+	);
+}
