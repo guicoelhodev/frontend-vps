c3a29d71402fdaa25456c19ea6f9d6ce25937b19
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-23 20:54:09 -0300

    feat: change title to test rebuild

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="grid w-screen h-screen place-content-center">
-			<h1 className="font-semibold text-3xl">HELLO VPS CLIENT !</h1>
+			<h1 className="font-semibold text-3xl">HELLO VPS CLIENT ! 2.</h1>
 		</div>
 	);
 }
