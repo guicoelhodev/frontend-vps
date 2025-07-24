64caf8fb810d6baa50c098b95a98bad1de941f67
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-23 21:28:45 -0300

    feat: change title to test rebuild 5

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid w-screen h-screen place-content-center">
       <h1 className="font-semibold text-3xl">HELLO VPS CLIENT ! 3.</h1>
-      <p>Running without use github actions</p>
+      <p>Running without use github actions .</p>
     </div>
   );
 }
