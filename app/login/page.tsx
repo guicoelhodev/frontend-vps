1c54bb9e46dd261f32560e79f2ddf31233db25cb
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-02 00:39:20 -0300

    feat(better-auth): create base better-auth implementation

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+
+	return (
+		<section className="p-4">
+			<p>login</p>
+		</section>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Task } from "./Task";
 
 export default function Home() {
+	const a: { name: number } = { name: 8}
 	return (
 		<div className="flex w-svw h-svh flex-col items-stretch justify-center">
 			<main className="flex flex-col items-center justify-center gap-4 size-full p-4">
